8676745bb2ce5d68d99e74644636ce127a9d0154
Paul McElroy <pcm@ku.edu>
2016-02-22 14:51:27 -0600

    Update css manipulation files

--- css_manipulation.js
+++ css_manipulation.js
@@ -1,17 +1,19 @@
 function backgroundChange() {
+  var mydiv = document.getElementById("dummy");
   var redV = document.getElementById("redText2").value;
   var greenV = document.getElementById("greenText2").value;
   var blueV = document.getElementById("blueText2").value;
-  document.getElementById(dummy) = rgb( redV, greenV, blueV) ;
+  mydiv.style.backgroundColor = rgb( redV, greenV, blueV);
 }
 
 function borderChange() {
-  var redV = parseInt(document.getElementById("redText1").value);
-  var greenV = parseInt(document.getElementById("greenText1").value);
-  var blueV = parseInt(document.getElementById("blueText1").value);
-  var widthV = parseInt(document.getElementById("widthText1").value);
-  document.body.style.border-color= 'rgb(' + redV + ',' + greenV + ',' + blueV + ')';
-  document.body.style.border-width= widthV;
+  var mydiv = document.getElementById("dummy");
+  var redV = document.getElementById("redText1").value;
+  var greenV = document.getElementById("greenText1").value;
+  var blueV = document.getElementById("blueText1").value;
+  var widthV = document.getElementById("widthText1").value;
+  mydiv.style.borderColor = 'rgb(' + redV + ',' + greenV + ',' + blueV + ')';
+  mydiv.style.width = widthV;
 }
 
 function rgb(r, g, b)
